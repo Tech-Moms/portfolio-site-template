3985e75a3e9c9bb318212a9c59b1946ff939d798
Adrienne Eror <40647605+ladyadi@users.noreply.github.com>
2021-12-17 22:27:00 -0700

    updated readme and spacing

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function setActiveLink() {
-    var links = document.getElementsByClassName("nav-link");
-    console.log(links.length)
-    for (var i = 0; i < links.length; i++) {
-        console.log(links[i])
-        if (links[i].innerHTML === 'Home') {
-            links[i].style = "text-decoration: underline";
-        }
+  var links = document.getElementsByClassName("nav-link");
+  console.log(links.length)
+  for (var i = 0; i < links.length; i++) {
+    console.log(links[i])
+    if (links[i].innerHTML === 'Home') {
+      links[i].style = "text-decoration: underline";
     }
+  }
 }
 
 setActiveLink();
