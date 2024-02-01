36f75b7193c6822480f3f2791d676a32acbafabb
juliecoding <julie.akohler@gmail.com>
2024-01-31 22:03:06 -0700

    Name tweak

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const greeting = document.getElementById("greeting")
 greeting.style.textAlign = "center"
 greeting.style.marginTop = "50px"
 
-const username = window.prompt("What's your name?", "Louisa May")
+const username = window.prompt("What's your name?", "Jane")
 greeting.innerHTML = "Welcome to my website, " + username + "!"
