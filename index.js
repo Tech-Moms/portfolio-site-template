7cfe900c9ab76d2e057d8dfd326b69340e04214e
juliecoding <julie.akohler@gmail.com>
2024-01-31 21:37:10 -0700

    Simplified JavaScript: greeting

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const experiment = document.getElementById("experiment");
+const greeting = document.getElementById("greeting")
+
+greeting.style.textAlign = "center"
+greeting.style.marginTop = "50px"
+
+const username = window.prompt("What's your name?", "Louisa May")
+greeting.innerHTML = "Welcome to my website, " + username + "!"
